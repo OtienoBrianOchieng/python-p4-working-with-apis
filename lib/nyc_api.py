01a04668ac985bb9cc2e329054e8c2f8a04082dd
root <root@DESKTOP-UEO62DE.localdomain>
2024-01-09 13:51:05 +0300

    complete URL assignmnet

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -11,4 +11,14 @@ class GetPrograms:
     return response.content
 
 programs = GetPrograms().get_programs()
-print(programs)
+
+def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+            programs_list.append(program["agency"])
+
+    return programs_list
+for school in set(programs_schools):
+    print(school)
